66add5f116a4bb9ccab77e7ee9adf3b1b3dfb0ac
revanth05-tech <revanthyegireddi@gmail.com>
2025-12-15 14:12:12 +0530

    Revise

---
+++ prep/jsprep/script.js
@@ -0,0 +1,25 @@
+// let name = "revanth";
+// let age = 21;
+
+// console.log(`my name is ${name}`)
+
+
+// let marks = 45;
+// if(marks > 40){
+//     console.log("pass")
+// }else if(marks < 40){
+//     console.log("fail")
+// }else{
+//     console.log("reappear")
+// }
+
+
+// for(let i =0;i<5;i++){
+//     console.log(i+1);
+// }
+
+// let count = 0;
+
+// for(let i = 0;i<=10;i++){
+//     console.log(count++)
+// }
