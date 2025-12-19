f8ec42bce8271d52b3440266baedfc05447272a6
revanth05-tech <revanthyegireddi@gmail.com>
2025-12-19 16:16:04 +0530

    revString

--- logicpractice/revStr.js
+++ logicpractice/revStr.js
@@ -1,6 +1,19 @@
-let a = "Hello World!";
-let b = "";
-for(let i = a.length -1;i>=0;i--){
-    b += a[i];
+// let a = "Hello World!";
+// let b = "";
+// for(let i = a.length -1;i>=0;i--){
+//     b += a[i];
+// }
+// console.log(b);
+
+
+const prompt = require("prompt-sync")();
+
+let str = String(prompt("Enter the String: "));
+
+let rev = "";
+
+for(let i = str.length -1;i>=0;i--){
+    rev+= str[i];
 }
-console.log(b);
+
+console.log("Reversed String is: "+rev);
